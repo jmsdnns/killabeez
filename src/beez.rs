8ec4c63f4f1b364e41409a56383cd11cc3aa7b8d
Jms Dnns <jdennis@gmail.com>
2025-02-26 21:12:40 -0500

    Changed how aws config is loaded. Also removed unnecessary dereferencing

--- src/beez.rs
+++ src/beez.rs
@@ -1,11 +1,10 @@
 use aws_sdk_ec2::{Client, Error};
-use tokio;
 
 use crate::aws;
 use crate::config::AppConfig;
 
 pub async fn mk_client(ac: &AppConfig) -> Result<Client, Error> {
-    let config = aws_config::load_from_env().await;
+    let config = aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await;
     Ok(Client::new(&config))
 }
 
@@ -18,21 +17,21 @@ pub struct AWSNetwork {
 impl AWSNetwork {
     async fn mk_network(client: &Client, ac: &AppConfig) -> Result<Self, Error> {
         // VPC
-        let Ok(vpc) = aws::create_vpc(&client, &ac).await else {
+        let Ok(vpc) = aws::create_vpc(client, ac).await else {
             panic!("[vpc] Waaaah");
         };
         let vpc_id = vpc.vpc_id.as_ref().unwrap();
         println!("VPC created: {:?}", vpc_id);
 
         // Subnet
-        let Ok(subnet) = aws::create_subnet(&client, vpc_id, &ac).await else {
+        let Ok(subnet) = aws::create_subnet(client, vpc_id, ac).await else {
             panic!("[subnet] Waaaah!");
         };
         let subnet_id = subnet.subnet_id.as_ref().unwrap();
         println!("Subnet created: {:?}", subnet_id);
 
         // Security Group
-        let Ok(sg_id) = aws::create_security_group(&client, vpc_id, &ac).await else {
+        let Ok(sg_id) = aws::create_security_group(client, vpc_id, ac).await else {
             panic!("[security_group] Waaaah!");
         };
         println!("Security Group created: {:?}", sg_id);
@@ -48,20 +47,20 @@ impl AWSNetwork {
 pub struct Swarm {}
 
 impl Swarm {
-    async fn mk_swarm(client: &Client, ac: AppConfig, network: AWSNetwork) -> Result<(), Error> {
+    async fn mk_swarm(client: &Client, ac: &AppConfig, network: &AWSNetwork) -> Result<(), Error> {
         // Key Pair
-        let Ok(key_pair_id) = aws::import_key_pair(&client, &ac).await else {
+        let Ok(key_pair_id) = aws::import_key_pair(client, ac).await else {
             panic!("[key pair] Waaaah!");
         };
         println!("Key Pair created: {:?}", key_pair_id);
 
         // EC2 Instances
         let Ok(instance_ips) = aws::create_instances(
-            &client,
+            client,
             &network.vpc_id,
             &network.subnet_id,
             &network.security_group_id,
-            &ac,
+            ac,
         )
         .await
         else {
