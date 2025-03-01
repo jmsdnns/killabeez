1142d28e27fdf4aa949d9f695b840e7b3ccf4600
Jms Dnns <jdennis@gmail.com>
2025-03-01 02:44:07 -0500

    Expanded cli while working on implementing commands. Def a WIP

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,8 @@
 use clap::{Parser, Subcommand};
 
-use crate::beez;
+use crate::aws::{ec2, tagged};
 use crate::config::AppConfig;
+use crate::scenarios::{AWSNetwork, Swarm};
 
 #[derive(Debug, Parser)]
 #[command(about = "A CLI for killabeez", long_about = None)]
@@ -21,6 +22,15 @@ enum Commands {
         count: i32,
     },
 
+    Network {
+        #[arg(required = true)]
+        name: String,
+    },
+
+    Tagged {
+        name: String,
+    },
+
     Terminate {
         #[arg(required = true)]
         name: String,
@@ -35,23 +45,32 @@ enum Commands {
     },
 }
 
-pub async fn run(ac: AppConfig) {
+pub async fn run(ac: &AppConfig) {
     let args = Cli::parse();
 
-    let Ok(client) = beez::mk_client(&ac).await else {
-        panic!("[client] Waaaah");
+    let Ok(client) = ec2::mk_client(ac).await else {
+        panic!("[cli] error: mk_client");
     };
 
     match args.command {
         Commands::Init { name, count } => {
-            println!("[init] {name}");
-            println!("[init] {:?}", count);
+            println!("[cli init] {name}");
+            let network = AWSNetwork::load_network(&client, ac).await.unwrap();
+            let swarm = Swarm::init_swarm(&client, ac, &network).await.unwrap();
+        }
+        Commands::Network { name } => {
+            println!("[cli network] {name}");
+            AWSNetwork::load_network(&client, ac).await;
+        }
+        Commands::Tagged { name } => {
+            println!("[cli tagged] {name}");
+            tagged::all_beez_tags().await;
         }
         Commands::Terminate { name } => {
-            println!("[terminate] {name}");
+            println!("[cli terminate] {name}");
         }
         Commands::Exec { name, script } => {
-            println!("[exec] {name} {:?}", script);
+            println!("[cli exec] {name} {:?}", script);
         }
     }
 }
