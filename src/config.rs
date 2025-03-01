de8b5d716ee9f207a1f33962ad5011b8c24aea57
Jms Dnns <jdennis@gmail.com>
2025-03-01 02:41:02 -0500

    Expanded the config to allow specifying vpc_id, subnet_id, security_group_id, and ami

--- src/config.rs
+++ src/config.rs
@@ -7,25 +7,37 @@ use figment::{
     providers::{Env, Format, Toml},
 };
 
+const DEFAULT_SSH_CIDR: &str = "0.0.0.0/0";
+const DEFAULT_AMI: &str = "ami-0c55b159cbfafe1f0";
+
 #[derive(Debug, Deserialize)]
 pub struct AppConfig {
     pub username: String,
     pub key_file: String,
     pub tag_name: String,
     pub num_beez: i32,
-    pub cidr_block: Option<String>,
+    pub vpc_id: Option<String>,
+    pub ssh_cidr_block: Option<String>,
+    pub subnet_id: Option<String>,
+    pub security_group_id: Option<String>,
+    pub ami: Option<String>,
 }
 
 impl AppConfig {
     pub fn read(filename: &str) -> Result<Self, Error> {
         let mut ac: AppConfig = Figment::new().merge(Toml::file(filename)).extract()?;
 
-        // TODO kinda risky
-        let cidr_block = match &ac.cidr_block {
-            None => "0.0.0.0/24".to_string(),
+        let ssh_cidr_block = match &ac.ssh_cidr_block {
+            None => DEFAULT_SSH_CIDR.to_string(),
             Some(cb) => cb.clone(),
         };
-        ac.cidr_block = Some(cidr_block);
+        ac.ssh_cidr_block = Some(ssh_cidr_block);
+
+        let ami = match &ac.ami {
+            None => DEFAULT_AMI.to_string(),
+            Some(ami) => ami.to_string(),
+        };
+        ac.ami = Some(ami);
 
         Ok(ac)
     }
