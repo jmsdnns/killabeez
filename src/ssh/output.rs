6505a169090c893765f4eb4933b42ca85051ef4c
Jms Dnns <jdennis@gmail.com>
2025-04-28 00:00:15 -0400

    Converted output to a system that configures how output is done both remotely and locally. Remote output stores stdout and stderr in files on the remote server. Local output streams stdout and stderr into two local files, per host.

--- src/ssh/output.rs
+++ src/ssh/output.rs
@@ -5,41 +5,18 @@ use std::sync::{Arc, Mutex, MutexGuard};
 
 /// A trait focused on managing stdout and stderr
 pub trait OutputHandler: Send + Sync {
+    /// ID for host that implementation is handling output for
     fn host_id(&self) -> &str;
+
+    /// Processes streams of stdout from remote host
     fn stdout(&self, data: &[u8]) -> io::Result<()>;
+
+    /// Processes streams of stderr from remote host
     fn stderr(&self, data: &[u8]) -> io::Result<()>;
+
+    /// Function that modifies a command string before execution
     fn update_command(&self, command: &str) -> String {
         String::from(command)
-    }
-}
-
-/// Basic console output
-pub struct ConsoleLogger {
-    host_id: String,
-}
-impl ConsoleLogger {
-    pub fn new(host_id: &str) -> io::Result<Self> {
-        Ok(Self {
-            host_id: host_id.to_string(),
-        })
-    }
-}
-
-impl OutputHandler for ConsoleLogger {
-    fn host_id(&self) -> &str {
-        &self.host_id
-    }
-
-    fn stdout(&self, data: &[u8]) -> io::Result<()> {
-        std::io::stdout().write_all(data).unwrap();
-        std::io::stdout().flush().unwrap();
-        Ok(())
-    }
-
-    fn stderr(&self, data: &[u8]) -> io::Result<()> {
-        std::io::stderr().write_all(data).unwrap();
-        std::io::stderr().flush().unwrap();
-        Ok(())
     }
 }
 
@@ -49,27 +26,27 @@ pub struct RemoteFiles {
     host_id: String,
     out_path: PathBuf,
     err_path: PathBuf,
+    verbose: bool,
 }
-impl RemoteFiles {
-    pub fn new(
-        host_id: &str,
-        out_path: Option<PathBuf>,
-        err_path: Option<PathBuf>,
-    ) -> io::Result<Self> {
-        let out_path = match out_path {
-            Some(path) => path,
-            None => PathBuf::from("stdout.log"),
-        };
 
-        let err_path = match err_path {
-            Some(path) => path,
-            None => PathBuf::from("stderr.log"),
+impl RemoteFiles {
+    pub fn new(host_id: &str, log_root: Option<&PathBuf>, verbose: bool) -> io::Result<Self> {
+        let stdout = PathBuf::from("stdout.log");
+        let stderr = PathBuf::from("stderr.log");
+
+        let (out_path, err_path) = match log_root {
+            Some(root) => (
+                PathBuf::from_iter([root.clone(), stdout]),
+                PathBuf::from_iter([root.clone(), stderr]),
+            ),
+            None => (stdout, stderr),
         };
 
         Ok(Self {
             host_id: host_id.to_string(),
             out_path,
             err_path,
+            verbose,
         })
     }
 }
@@ -79,47 +56,70 @@ impl OutputHandler for RemoteFiles {
         &self.host_id
     }
 
+    /// stdout is not sent from remote unless verbose flag is used
     fn stdout(&self, data: &[u8]) -> io::Result<()> {
+        if self.verbose {
+            std::io::stderr().write_all(data).unwrap();
+            std::io::stderr().flush().unwrap();
+        }
+
         Ok(())
     }
 
+    /// stderr is not sent from remote unless verbose flag is used
     fn stderr(&self, data: &[u8]) -> io::Result<()> {
+        if self.verbose {
+            std::io::stderr().write_all(data).unwrap();
+            std::io::stderr().flush().unwrap();
+        }
+
         Ok(())
     }
 
-    fn update_command(&self, command: &str) -> String {
-        let redirect_out = format!(
-            r#"awk '{{ print strftime("[%Y-%m-%d %H:%M:%S] "), $0 }}' > {}"#,
+    /// Wraps a command string with code that adds timestamps to output and redirects
+    /// both stdout and stderr to files. Output is written to both files and the console
+    /// if `verbose` is true
+    fn update_command(&self, cmd: &str) -> String {
+        let outh = format!(
+            r#"awk '{{ print strftime("[%Y-%m-%d %H:%M:%S] {} "), $0, "" }}' >> {}"#,
+            &self.host_id(),
             &self.out_path.display()
         );
-        let redirect_err = format!(
-            r#"awk '{{ print strftime("[%Y-%m-%d %H:%M:%S] "), $0 }}' > {}"#,
+
+        let errh = format!(
+            r#"awk '{{ print strftime("[%Y-%m-%d %H:%M:%S] {}"), $0, "" }}' >> {}"#,
+            &self.host_id(),
             &self.err_path.display()
         );
 
-        // NOTE: logs are appended with `>>`
-        format!("{} >> >({}) 2>> >({})", command, redirect_out, redirect_err)
+        if self.verbose {
+            // NOTE: this feels hacky, but it works too
+            format!("{} > >(tee >({}) >&1) 2> >(tee >({}) >&2)", cmd, outh, errh)
+        } else {
+            format!("{} > >({}) 2> >({})", cmd, outh, errh)
+        }
     }
 }
 
-/// Write remote stdout & stderr to local files as it is streamed from each machine
+/// Threadsafe struct that handles writing output streams to local files
 pub struct StreamLogger {
     host_id: String,
     stdout_file: Arc<Mutex<File>>,
     stderr_file: Arc<Mutex<File>>,
+    verbose: bool,
 }
+
 impl StreamLogger {
-    pub fn new(host_id: &str, log_dir: &Path) -> io::Result<Self> {
+    pub fn new(host_id: &str, log_dir: &Path, verbose: bool) -> io::Result<Self> {
         std::fs::create_dir_all(log_dir)?;
 
         let stdout_path = log_dir.join(format!("{}_stdout.log", host_id));
-        let stderr_path = log_dir.join(format!("{}_stderr.log", host_id));
-
         let stdout_file = OpenOptions::new()
             .create(true)
             .append(true)
             .open(stdout_path)?;
 
+        let stderr_path = log_dir.join(format!("{}_stderr.log", host_id));
         let stderr_file = OpenOptions::new()
             .create(true)
             .append(true)
@@ -129,6 +129,7 @@ impl StreamLogger {
             stdout_file: Arc::new(Mutex::new(stdout_file)),
             stderr_file: Arc::new(Mutex::new(stderr_file)),
             host_id: host_id.to_string(),
+            verbose,
         })
     }
 
@@ -136,6 +137,7 @@ impl StreamLogger {
         let timestamp = chrono::Local::now()
             .format("[%Y-%m-%d %H:%M:%S] ")
             .to_string();
+
         file.write_all(timestamp.as_bytes())?;
         file.write_all(data)?;
         if !data.ends_with(b"\n") {
@@ -150,13 +152,29 @@ impl OutputHandler for StreamLogger {
         &self.host_id
     }
 
+    /// writes remote stdout to local file as it is streamed. will write copy
+    /// to console if `verbose` is true
     fn stdout(&self, data: &[u8]) -> io::Result<()> {
         let mut file = self.stdout_file.lock().unwrap();
-        self.log_to_file(&mut file, data)
+        self.log_to_file(&mut file, data)?;
+
+        if self.verbose {
+            std::io::stderr().write_all(data).unwrap();
+            std::io::stderr().flush().unwrap();
+        }
+
+        Ok(())
     }
 
+    /// writes remote stderr to local file as it is streamed. will write copy
+    /// to console if `verbose` is true
     fn stderr(&self, data: &[u8]) -> io::Result<()> {
         let mut file = self.stderr_file.lock().unwrap();
-        self.log_to_file(&mut file, data)
+        self.log_to_file(&mut file, data)?;
+
+        std::io::stderr().write_all(data).unwrap();
+        std::io::stderr().flush().unwrap();
+
+        Ok(())
     }
 }
